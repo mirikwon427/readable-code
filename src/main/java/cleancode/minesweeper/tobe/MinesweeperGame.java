a47fc5c9524ddcb51078d8030401c41ebb569885
권미리 <miri@gwonmiliui-MacBookPro.local>
2025-03-07 02:02:41 +0900

    Refactor: 확정된 값(상수)에서 메서드 호출하도록 로직 개선

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -128,7 +128,7 @@ public class MinesweeperGame {
     private static boolean isAllCellOpened() {
         return Arrays.stream(BOARD)
             .flatMap(Arrays::stream)
-            .noneMatch(cell -> cell.equals(CLOSED_CELL_SIGN));
+            .noneMatch(CLOSED_CELL_SIGN::equals);
     }
 
     private static int convertRowFrom(char CellInputRow) {
